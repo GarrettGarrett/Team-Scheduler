871abae9da0d13179a7d9ba9be6e9d7ab8835a11
PyPy <pythonautomatefinance@gmail.com>
2021-07-28 18:48:44 -0700

    Adds update class

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.home)
+    path('', views.home),
+    path('new/', views.ScheduleCreate.as_view()),
+    path('<int:pk>/update', views.ScheduleUpdate.as_view()),
+    path('<int:pk>/delete', views.ScheduleDelete.as_view()),
 
 ]
