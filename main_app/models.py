843d38e7e233435c6c84427b8b7415d422c32ec2
PyPy <pythonautomatefinance@gmail.com>
2021-07-28 18:27:29 -0700

    adds Schedule models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Schedule(models.Model):
+    name = models.CharField(max_length=100)
+    SundayStart = models.IntegerField()
+    SundayEnd = models.IntegerField()
+
+    def __str__(self):
+        return self.name
