ac2b7cd893b495c617c6839b0a333a794e171a61
PyPy <pythonautomatefinance@gmail.com>
2021-07-28 19:13:17 -0700

    Adds login functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Schedule(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
     SundayStart = models.IntegerField(default='0', verbose_name='Sunday Start Time')
