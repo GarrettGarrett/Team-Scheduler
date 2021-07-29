cda5cc35190d85bb29bfbf9c8e059d709daf9fb6
PyPy <pythonautomatefinance@gmail.com>
2021-07-28 18:56:47 -0700

    cleans url.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    # path('new/', views.new),
     path('new/', views.ScheduleCreate.as_view()),
     path('<int:pk>/update', views.ScheduleUpdate.as_view()),
     path('<int:pk>/delete', views.ScheduleDelete.as_view()),
